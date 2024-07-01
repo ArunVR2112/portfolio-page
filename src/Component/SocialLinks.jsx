907f9d7705fe2d835f8abaee05b362d4c537d8cf
ArunVR2112 <arunvrathod2000@gmail.com>
2024-07-01 11:44:03 +0530

    download link fixed

--- src/Component/SocialLinks.jsx
+++ src/Component/SocialLinks.jsx
@@ -1,16 +1,10 @@
-import React from 'react'
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
-import { HiOutlineMail } from 'react-icons/hi'
-import { BsFillPersonLinesFill } from 'react-icons/bs'
-
-
-
-
+import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const SocialLinks = () => {
-
     const links = [
-
         {
             id: 1,
             link: 'https://linkedin.com/',
@@ -18,14 +12,12 @@ const SocialLinks = () => {
             icon: <FaLinkedin size={30} />,
             style: "rounded-tr-md"
         },
-
         {
             id: 2,
             link: 'https://github.com/ArunVR2112',
             nameofTag: 'Github',
             icon: <FaGithub size={30} />
-        }
-        ,
+        },
         {
             id: 3,
             link: 'mailto:arunvrathod@gmail.com',
@@ -35,32 +27,28 @@ const SocialLinks = () => {
         },
         {
             id: 4,
-            link: '../Assets/Java_Developer.pdf',
-            nameofTag: "PortFolio",
+            link: '/Java_Developer.pdf',
+            nameofTag: "Portfolio",
             icon: <BsFillPersonLinesFill size={30} />,
-            dounload: true
+            download: true
         }
-    ]
+    ];
+
     return (
-        <div className="hidden  lg:flex flex-col  left-0 fixed" style={{ top: "35%" }}>
+        <div className="hidden lg:flex flex-col left-0 fixed" style={{ top: "35%" }}>
             <ul>
-                {
-                    links.map(({ id, link, nameofTag, icon, download }) => (
-                        <li key={id} className='flex justify-center items-center h-10 w-40 px-4 ml-[-100px] hover:rounded-md duration-300 hover:ml-[-10px] bg-gray-500'>
-                            <a href={link} download={download} target='_blank' rel="noreferrer" className='flex justify-between items-center w-full text-white rounded-md' >
-                                <>
-                                    {nameofTag}{icon}
-                                </>
-                            </a>
-                        </li>
-                    ))
-                }
-
-
+                {links.map(({ id, link, nameofTag, icon, download }) => (
+                    <li key={id} className='flex justify-center items-center h-10 w-40 px-4 ml-[-100px] hover:rounded-md duration-300 hover:ml-[-10px] bg-gray-500'>
+                        <a href={process.env.PUBLIC_URL + link} download={download} target='_blank' rel="noopener noreferrer" className='flex justify-between items-center w-full text-white rounded-md'>
+                            <>
+                                {nameofTag}{icon}
+                            </>
+                        </a>
+                    </li>
+                ))}
             </ul>
-
         </div>
-    )
-}
+    );
+};
 
-export default SocialLinks
+export default SocialLinks;
